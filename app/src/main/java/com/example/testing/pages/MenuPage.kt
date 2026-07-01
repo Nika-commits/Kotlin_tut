688f57e28462582367aa6c79e25edde8e08ffc40
Your Name <itismepranish@gmail.com>
2026-07-01 11:06:17 +0545

    chore: added Card Component

--- app/src/main/java/com/example/testing/pages/MenuPage.kt
+++ app/src/main/java/com/example/testing/pages/MenuPage.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,7 +34,16 @@ fun MenuPage() {
 
     LazyColumn {
         items(5){
-            ProductItem(demo, onAdd = ::onAdd)
+            Card(
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 2.dp
+                ),
+                modifier = Modifier
+                    .padding(8.dp)
+
+            ) {
+                ProductItem(demo, onAdd = ::onAdd)
+            }
         }
     }
 
