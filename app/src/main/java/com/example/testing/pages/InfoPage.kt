3ad68d74c4975f81e3e2b586297cb966046fb27e
Your Name <itismepranish@gmail.com>
2026-07-02 14:25:35 +0545

    fix: removed duplicate Info Page

--- app/src/main/java/com/example/testing/pages/InfoPage.kt
+++ app/src/main/java/com/example/testing/pages/InfoPage.kt
@@ -1,19 +1,25 @@
 package com.example.testing.pages
 
 import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.testing.Profile
 import com.example.testing.R
 
 @Preview
 @Composable
  fun InfoPage() {
+     val profile = Profile("Pranish Chaulagain", "pranish@gmail.com", 2, "rr")
     AndroidView(
         factory = {context ->
             LayoutInflater.from(context)
                 .inflate(R.layout.info_page, null)
+        },
+        update = { view ->
+            view.findViewById<TextView>(R.id.tvName).text = profile.name
         }
     )
 }
