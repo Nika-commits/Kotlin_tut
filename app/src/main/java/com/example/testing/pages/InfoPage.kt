6a9ee068be809a704feff9076b288a021b18f1da
Your Name <itismepranish@gmail.com>
2026-07-02 12:29:15 +0545

    chore: create Info Page xml and embed to info page function with AndroidView

--- app/src/main/java/com/example/testing/pages/InfoPage.kt
+++ app/src/main/java/com/example/testing/pages/InfoPage.kt
@@ -1,11 +1,19 @@
 package com.example.testing.pages
 
+import android.view.LayoutInflater
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.testing.R
 
 @Preview
 @Composable
-public fun InfoPage() {
-    Text("Info Page")
+ fun InfoPage() {
+    AndroidView(
+        factory = {context ->
+            LayoutInflater.from(context)
+                .inflate(R.layout.info_page, null)
+        }
+    )
 }
